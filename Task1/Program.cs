ade7c7e8d493ff53242f58c2a46016aed62b71f7
ndzd <n_level@mail.ru>
2022-09-09 07:47:14 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+
+// 78 -> 2
+
+// 89126 -> 5
+System.Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int NumberOfDigits(int a){
+    
+int count = 1;
+while(a > 9 || a < -9){
+    a = a / 10;
+    count++;
+    }
+return count;
+}
+
+
+System.Console.WriteLine(NumberOfDigits(a));
---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+
+// [1,0,1,1,0,1,0,0]
+
+int[] GetRandomArray(){
+    int [] arr = new int [8];
+           
+    for (int i = 0; i < 8; i++){
+        arr[i] = new Random().Next(0, 2);
+        
+    }
+    return arr;
+}
+
+
+System.Console.WriteLine($"[{string.Join(", ", GetRandomArray())}]");
+
+
+
+
+
+
+
+
+// int[] array = GetRandomArray();
+// System.Console.Write("[");
+// foreach (var item in array)
+// {
+//     System.Console.Write($"{item}" + " " );
+// }
+// System.Console.Write("]");
+// System.Console.Write("[1, ");
+// System.Console.Write("2");
+// System.Console.Write(", 3");
